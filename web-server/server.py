92049fad847fda5c56040f47342d41ef5f9cd9d3
Arnie <arnie752@gmail.com>
2024-04-28 18:44:01 +0300

    feat(Task 1): Created a basic web server using Flask

---
+++ web-server/server.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/home', methods=['GET'])
+def home():
+    server_id = os.environ.get('SERVER_ID', 'Unknown')
+    message = f"Hello from Server: {server_id}"
+    return {"message": message, "status": "successful"}, 200
+
+@app.route('/heartbeat', methods=['GET'])
+def heartbeat():
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
